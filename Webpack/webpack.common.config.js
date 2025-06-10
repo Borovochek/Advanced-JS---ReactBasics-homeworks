499ee47fdb4a3d31b57b8ce873f769e9697706df
Ant <jabbahatit@gmail.com>
2025-06-10 19:42:32 +0300

    Initial commit

---
+++ Webpack/webpack.common.config.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'styles.css'
+    })
+  ],
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, 'public'),
+    },
+    open: true,
+    hot: true,
+    port: 8080,
+  }
+};
