be65d83694ee52b1bf288cf871e9acf5c453cc88
Ant <jabbahatit@gmail.com>
2025-07-03 12:49:19 +0300

    Преобразование react-components из подмодуля в обычную папку

---
+++ react-components/my-store-func/src/App.js
@@ -0,0 +1,40 @@
+import './components/store-func/css/main.css';
+import { item } from './components/store-func/data.js';
+import ShopItemFunc from './components/store-func/ShopItemFunc.js';
+
+function App() {
+  return (
+  <div className="container">
+    <div className="background-element">
+    </div>
+    <div className="highlight-window">
+      <div className='highlight-overlay'></div>
+    </div>
+    <div className="window">
+      <ShopItemFunc item={item} />
+    </div>
+  </div>
+)
+}
+
+export default App;
+
+
+// return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
